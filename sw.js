591afb4a6ecbf3dbe39d9cc04a379ff759dda1ee
Torri <torrigunn@gmail.com>
2026-08-20 09:02:32 -0400

    Add undo + day browsing (v2)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cal365-v1";
+const CACHE = "cal365-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
